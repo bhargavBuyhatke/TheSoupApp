579481278d517a49f792a8cd4978b4fbf46103ab
janipasha <janipasha1308@gmail.com>
2017-04-09 17:05:11 +0530

    Implemented StoryFeedAdapter

--- StoryData.java
+++ StoryData.java
@@ -16,6 +16,35 @@ public class StoryData {
     String article_image_url;
     String article_title;
     String num_articles;
+    String latest_substory;
+
+
+    public String getStoryName() {
+
+        return story_name;
+
+    }
+
+    public String getSubStoryName() {
+        return substory_name;
+    }
+
+    public String getNumArticle() {
+        return num_articles;
+    }
+
+    public String getTime() {
+        return latest_substory;
+    }
+
+    public String getImageUrl() {
+        return image_url;
+    }
+
+    public String getArticleImageUrl(){
+        return article_image_url;
+    }
+
 
 }
 
